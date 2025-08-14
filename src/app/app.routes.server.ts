095536ad0a3b4079aa638b7bafcd839a0ecca5c4
duclee204 <BIT220032@st.cmcu.edu.vn>
2025-08-14 21:06:39 +0700

    Frontend Deploy

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'exam-result/:attemptId',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
